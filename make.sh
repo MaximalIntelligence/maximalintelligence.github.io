5565b80e77defe577b718014945918734ff129ae
Sven Voigt <svenpvoigt@gmail.com>
2026-09-15 02:08:24 -0400

    favicon

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@ rm docs/*
 cp logo.svg docs/logo.svg
 cp style.css docs/style.css
 cp script.js docs/script.js
-pandoc index.md -s --css=style.css -o docs/index.html
+pandoc index.md -s -H favicon-header.html --css=style.css -o docs/index.html
